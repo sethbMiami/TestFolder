5202958848ba53ab9c3c58130ada8e9d5f7e68b4
seth bruner <bruner.seth@outlook.com>
2021-06-27 19:52:28 -0400

    built a class with 2 methods and added text to REAMME

--- README.txt
+++ README.txt
@@ -3,3 +3,10 @@ This is the start of my programming journey. I am on week 3 and am taking some C
 This is my first time using Git, Github, and creating programs in python. 
 
 Not sure what else to put in this README, but these commits will most likely be just practice files.
+
+
+
+Whitepages.com API
+Input list of addresses
+Street, zip
+Return the first listed name (first, last) for each address
--- pythonClassPractice.py
+++ pythonClassPractice.py
@@ -2,5 +2,40 @@
 
 class Employee:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = self.last + self.first + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def salary(self):
+        return '{} current salary is {}'.format(self.first, self.pay)
+
+
+emp_1 = Employee('Seth', 'Bruner', 61000)
+emp_2 = Employee('Emily', 'Mendez', 65000)
+emp_3 = Employee('Test', 'User', 50000)
+emp_4 = Employee('Kris', 'Acevedo', 45000)
+emp_5 = Employee('Chris', 'Leyva', 80000)
+
+
+# print(emp_1)
+# print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+print(emp_5.email)
+
+print(emp_1.fullname())
+print(emp_2.fullname())
+print(emp_5.fullname())
+
+
+print(emp_1.salary())
+print(emp_2.salary())
+print(emp_5.salary())
+
+
